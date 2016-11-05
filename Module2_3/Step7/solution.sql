0acf04868e651b83020ddcafb2e587bf8cbbe336
igorxut <igorxut@gmail.com>
2016-11-05 16:29:29 +0300

    Create solution.sql

---
+++ Module2_3/Step7/solution.sql
@@ -0,0 +1,13 @@
+SELECT
+    good.name AS good_name,
+    category.name AS category_name
+FROM
+    good
+LEFT OUTER JOIN
+    category_has_good
+ON
+    good.id = category_has_good.good_id
+LEFT OUTER JOIN
+    category
+ON
+    category_has_good.category_id = category.id;
